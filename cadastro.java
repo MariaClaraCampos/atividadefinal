68ed773bb5c57b5c5f3ca52f29646301fccee3a0
Maria Clara <84872400+MariaClaraCampos@users.noreply.github.com>
2021-12-02 11:30:26 -0300

    Model

---
+++ cadastro.java
@@ -0,0 +1,21 @@
+package br.com.login.models;
+
+public class cadastro {
+
+	private String nomePessoa;
+	private String nomeDisciplina;
+	
+	public String getNomePessoa() {
+		return nomePessoa;
+	}
+	public void setNomePessoa(String nomePessoa) {
+		this.nomePessoa = nomePessoa;
+	}
+	
+	public String getNomeDisciplina() {
+		return nomeDisciplina;
+	}
+	public void setNomeDisciplina(String nomeDisciplina) {
+		this.nomeDisciplina = nomeDisciplina;
+	}
+}
