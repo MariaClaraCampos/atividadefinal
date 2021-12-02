8fc06b329e67b41de7fd5f7a2c67077a9a2a3e25
Maria Clara <84872400+MariaClaraCampos@users.noreply.github.com>
2021-12-02 12:13:40 -0300

    model

---
+++ login.java
@@ -0,0 +1,22 @@
+package br.com.login.models;
+
+public class login {
+
+	private String Usuario;
+	private String Senha;  
+	
+	public String getUsario() {
+		return Usuario;
+	}
+	public void setUsuario(String Usuario) {
+		this.Usuario = Usuario;
+	}	
+	public String getSenha() {
+		return Senha;
+	}
+	public void setSenha(String Senha) {
+		this.Senha = Senha; 
+	}
+}
+
+
