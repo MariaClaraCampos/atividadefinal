3b3309a0c6fb0e692aa2dd050fd918025e1f0b3e
Maria Clara <84872400+MariaClaraCampos@users.noreply.github.com>
2021-12-02 11:29:58 -0300

    Model

---
+++ menu.java
@@ -0,0 +1,38 @@
+package br.com.login.models;
+
+public class menu {
+	
+	private String Consultar;
+	private String Disciplinas;
+	private String Cadastro;
+	private String Header;
+	
+	public String getConsultar() {
+		return Consultar;
+	}
+	public void setConsultar(String consultar) {
+		Consultar = consultar;
+	}
+	
+	public String getDisciplinas() {
+		return Disciplinas;
+	}
+	public void setDisciplinas(String disciplinas) {
+		Disciplinas = disciplinas;
+	}
+	
+	public String getCadastro() {
+		return Cadastro;
+	}
+	public void setCadastro(String cadastro) {
+		Cadastro = cadastro;
+	}
+	
+	public String getHeader() {
+		return Header;
+	}
+	public void setHeader(String header) {
+		Header = header;
+	}
+
+}
